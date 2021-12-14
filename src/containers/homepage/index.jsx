3e4d48ae0e3e3e1cc51cb03e29474dd8d44b2588
Anele <anele.sigenu@playsafesa.com>
2021-12-14 15:55:46 +0200

    Initial commit for Isonka

---
+++ src/containers/homepage/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import { Footer } from "../../components/footer";
+import { Marginer } from "../../components/marginer";
+import { MoreAboutSection } from "./moreAboutSection";
+import { ReviewsSection } from "./reviewsSection";
+import { ServicesSection } from "./servicesSection";
+import { TopSection } from "./topSection";
+import { ImageSlider }  from "../../components/imageSlider"
+import { SliderData } from '../../components/imageSlider/SliderData.js';
+
+const PageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+const GalleryText = styled.h3`
+  font-size: 34px;
+  font-weight: 500;
+  line-height: 1.4;
+  margin: 0;
+  text-align: center;
+`;
+const GalleryDescription = styled.p`
+  color: #7a7a7a;
+  font-size: 21px;
+  text-align: center;
+  max-width: 100%;
+  margin-block: auto;
+`;
+export function Homepage(props) {
+  return (
+    <PageContainer>
+      <TopSection />
+      {/* <ServicesSection /> */}
+      {/* <GalleryText>GET INSPIRED</GalleryText> */}
+      {/* <GalleryDescription>We’ve converted hundreds of dream vehicles for our clients. 
+        Explore these fully custom Peak vehicles and start dreaming.</GalleryDescription>
+      <ImageSlider slides={SliderData} />  */}
+      {/* <ReviewsSection /> */}
+      {/* <MoreAboutSection /> */}
+      {/* <Marginer direction="vertical" margin="8em" /> */}
+      <Footer />
+    </PageContainer>
+  );
+}
---
+++ src/theme.js
@@ -0,0 +1,3 @@
+export const theme = {
+  primary: "#eb9e2b",
+};
